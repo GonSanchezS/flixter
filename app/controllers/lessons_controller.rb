bab5f0b6aa0e4c6f3f2510b30ee3456468881605
Gonzalo <gonsanchezs@gmail.com>
2016-09-02 19:39:13 +0000

    Secure aws credentials

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,7 +2,6 @@ class LessonsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_lesson
 
-
   def show
   end
 
@@ -14,10 +13,8 @@ class LessonsController < ApplicationController
     end
   end
 
-
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
-
 end
